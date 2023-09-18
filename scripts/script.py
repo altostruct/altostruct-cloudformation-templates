c7e7a61dd00fd06a907e885e55fdd3c50b727ba7
Mustafa Ali <mustaafaa.aali@gmail.com>
2023-09-18 10:32:36 +0200

    Added logic to script

--- scripts/script.py
+++ scripts/script.py
@@ -5,6 +5,8 @@ for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         filename = os.path.join(subdir, file)
         if filename.endswith(".yml") or filename.endswith(".json"): 
-            test = os.system(f'aws cloudformation validate-template --template-body file://{filename}')
-            print(test)
-            print(filename)
+            error = os.system(f'aws cloudformation validate-template --template-body file://{filename}')
+            if(error != 0):
+                print("There is a syntax error in the template named: ", filename)
+                exit(1)
+                            
